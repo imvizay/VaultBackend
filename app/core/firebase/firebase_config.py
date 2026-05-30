5bdb73842afe063a5dbfab1a3ad00b1ab1543abf
imvizay <vizzaymeena@gmail.com>
2026-05-30 15:00:13 +0530

    modified firebase credential path config

--- app/core/firebase/firebase_config.py
+++ app/core/firebase/firebase_config.py
@@ -1,11 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
 
 print("INITIALIZING FIREBASE")
 
-cred = credentials.Certificate(
-    'app/core/firebase/firebase_admin.json'
+firebase_path = os.getenv(
+    "FIREBASE_CREDENTIAL_PATH",
+    "app/core/firebase/firebase_admin.json"
 )
+
+cred = credentials.Certificate(firebase_path)
 
 firebase_admin.initialize_app(cred)
 
